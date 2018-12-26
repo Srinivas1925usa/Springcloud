438710b5cd01f73bc48803d4b2eff5eb18ccda59
Srinivas <srinivas1925usa@gmail.com>
2018-12-25 18:34:30 -0600

    Feign Client executed

--- feignclient/src/main/java/com/motubo/feignclient/interfaces/GreetingClient.java
+++ feignclient/src/main/java/com/motubo/feignclient/interfaces/GreetingClient.java
@@ -3,7 +3,7 @@ package com.motubo.feignclient.interfaces;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("eurekaclient")
+@FeignClient("eureka-client")
 public interface GreetingClient {
 	
 	@RequestMapping("/greeting")
